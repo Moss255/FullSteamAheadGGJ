bad83e335c48774a78f3e72e896b3ad78bdb72eb
Moss255 <32885261+Moss255@users.noreply.github.com>
2020-01-31 20:26:20 +0000

    First version of Carriage breaking and repair logic

---
+++ Carriage/Assets/breakdown.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class breakdown : MonoBehaviour
+{
+    public float health;
+    public int currentItemRequired;
+    public List<int> possibleItemsRequired;
+    public float degradeSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Selects item required (Is Random)
+
+        possibleItemsRequired = SearchItemLoaded();
+
+        //currentItemRequired = possibleItemsRequired[0];
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (health > 0)
+        {
+            health -= degradeSpeed;
+        }
+
+
+        if (health <= 0 && currentItemRequired == 0)
+        {
+            selectNewItem();
+        }
+    }
+
+    private List<int> SearchItemLoaded()
+    {
+
+        //// Look for items that are currently loaded and adds them to a list.
+        List<int> returnItems = new List<int>();
+
+        GameObject[] possibleItems = GameObject.FindGameObjectsWithTag("Item");
+        foreach (GameObject item in possibleItems)
+        {
+            returnItems.Add(item.GetComponent<itemInfo>().ID);
+        }
+
+        return returnItems;
+    }
+
+    private void selectNewItem()
+    {
+        currentItemRequired = possibleItemsRequired[Random.Range(0, possibleItemsRequired.Count)];
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        GameObject itemToCheck = collision.collider.gameObject;
+            
+        if (itemToCheck.CompareTag("Item"))
+        {
+            if (currentItemRequired == itemToCheck.GetComponent<itemInfo>().ID)
+            {
+                Destroy(itemToCheck);
+                health = 200f;
+            }
+        }
+    }
+}
---
+++ Carriage/Assets/itemInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemInfo : MonoBehaviour
+{
+    public int ID;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ID = Random.Range(1, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
